f51ad5c3cc61e8b520132c16e07b0c577a1ae28f
MD_Developer <kiprotichd@kabarak.ac.ke>
2021-07-27 10:45:50 +0300

    Animejs animations by brad traversy medai

--- main.js
+++ main.js
@@ -1,59 +1,15 @@
-// anime({
-//   targets: 'div.box.red',
-//   translateY: [
-//     { value: 200, duration: 500 },
-//     { value: 0, duration: 800 }
-//   ],
-//   rotate:{
-//     value: '1turn',
-//     easing: 'easeInOutSine'
-//   }
-// });
-
-// anime({
-//   targets: 'div.box.blue',
-//   translateY: [
-//     { value: 200, duration: 500, delay: 1000},
-//     { value: 0, duration: 800 }
-//   ],
-//   rotate:{
-//     value: '1turn',
-//     easing: 'easeInOutSine',
-//     delay: 1000
-//   }
-// });
-
-// anime({
-//   targets: 'div.box.green',
-//   translateY: [
-//     { value: 200, duration: 500, delay: 2000},
-//     { value: 0, duration: 800 }
-//   ],
-//   rotate:{
-//     value: '1turn',
-//     easing: 'easeInOutSine',
-//     delay: 2000
-//   }
-// });
-
-// anime({
-//   targets: 'div.box.yellow',
-//   translateY: [
-//     { value: 200, duration: 500, delay: 3000},
-//     { value: 0, duration: 800 }
-//   ],
-//   rotate:{
-//     value: '1turn',
-//     easing: 'easeInOutSine',
-//     delay: 3000
-//   }
-// });
-
 var playPause = anime({
   targets: 'div.box',
   translateY: [
     { value: 200, duration: 500 },
     { value: 0, duration: 800 }
+
+  ],
+  translateX: [
+    { value: 200, duration: 500 },
+    { value: 200, duration: 800 },
+    { value: 0, duration:1200}
+    
   ],
   rotate:{
     value: '1turn',
